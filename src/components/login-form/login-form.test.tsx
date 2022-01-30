aeb8723500d3acdb63d0bb271470026e81baf106
miguelsndc <mglsndc@gmail.com>
2022-01-30 17:01:09 -0300

    test(forms): prevent the not wrapped in act warning

--- src/components/login-form/login-form.test.tsx
+++ src/components/login-form/login-form.test.tsx
@@ -1,57 +1,65 @@
-import { render, screen, waitFor } from '@testing-library/react';
-import user from '@testing-library/user-event';
-import LoginForm from '.';
+import { render, screen, waitFor } from '@testing-library/react'
+import user from '@testing-library/user-event'
+import LoginForm from '.'
 
 describe('components/login-form', () => {
-  const onSubmit = jest.fn();
+  const onSubmit = jest.fn()
 
   beforeEach(() => {
-    render(<LoginForm handleSubmit={onSubmit} />);
-  });
+    render(<LoginForm handleSubmit={onSubmit} />)
+  })
 
   it('should start with submit button disabled', () => {
-    const loginButton = screen.getByRole('button', { name: /login/i });
-    expect(loginButton).toBeDisabled();
-  });
+    const loginButton = screen.getByRole('button', { name: /login/i })
+    expect(loginButton).toBeDisabled()
+  })
 
   it('should allow users to type into the fields and submit the form', async () => {
-    const emailField = screen.getByLabelText(/email/i);
-    const passwordField = screen.getByLabelText(/password/i);
-    const loginButton = screen.getByRole('button', { name: /login/i });
+    const emailField = screen.getByLabelText(/email/i)
+    const passwordField = screen.getByLabelText(/password/i)
+    const loginButton = screen.getByRole('button', { name: /login/i })
 
-    user.type(emailField, 'john@email.com');
-    user.type(passwordField, 'fakePassword123');
+    user.type(emailField, 'john@email.com')
+    user.type(passwordField, 'fakePassword123')
 
-    user.click(loginButton);
+    user.click(loginButton)
 
     await waitFor(() => {
       expect(onSubmit).toHaveBeenCalledWith({
         email: 'john@email.com',
         password: 'fakePassword123',
-      });
-    });
-  });
+      })
+    })
+  })
 
   describe('with invalid inputs', () => {
-    beforeEach(async () => {
-      const emailField = screen.getByLabelText(/email/i);
-      const passwordField = screen.getByLabelText(/password/i);
+    const setupInvalidInputs = () => {
+      const emailField = screen.getByLabelText(/email/i)
+      const passwordField = screen.getByLabelText(/password/i)
 
-      await waitFor(() => user.type(emailField, 'gh'));
-      await waitFor(() => user.type(passwordField, 'gh'));
-    });
+      user.type(emailField, 'gh')
+      user.type(passwordField, 'gh')
+    }
 
-    it('should display error messages ', () => {
-      screen.getAllByRole('alert');
-    });
+    it('should display error messages ', async () => {
+      setupInvalidInputs()
 
-    it('should set the inputs aria-invalid to true', async () => {
-      expect(screen.getByLabelText(/email/i)).toBeInvalid();
-      expect(screen.getByLabelText(/password/i)).toBeInvalid();
-    });
+      await screen.findAllByRole('alert')
+    })
+
+    it('should make the inputs invalid', async () => {
+      setupInvalidInputs()
+
+      expect(await screen.findByLabelText(/email/i)).toBeInvalid()
+      expect(await screen.findByLabelText(/password/i)).toBeInvalid()
+    })
 
     it('should keep the button disabled', async () => {
-      expect(screen.getByRole('button', { name: /login/i })).toBeDisabled();
-    });
-  });
-});
+      setupInvalidInputs()
+
+      expect(
+        await screen.findByRole('button', { name: /login/i })
+      ).toBeDisabled()
+    })
+  })
+})
--- src/components/register-form/register-form.test.tsx
+++ src/components/register-form/register-form.test.tsx
@@ -1,33 +1,33 @@
-import { render, screen, waitFor } from '@testing-library/react';
-import user from '@testing-library/user-event';
-import RegisterForm from '.';
+import { render, screen, waitFor } from '@testing-library/react'
+import user from '@testing-library/user-event'
+import RegisterForm from '.'
 
 describe('components/register-form', () => {
-  const onSubmit = jest.fn();
+  const onSubmit = jest.fn()
 
   beforeEach(() => {
-    render(<RegisterForm handleSubmit={onSubmit} />);
-  });
+    render(<RegisterForm handleSubmit={onSubmit} />)
+  })
 
   it('should start with submit button disabled', () => {
-    const submitButton = screen.getByRole('button', { name: /submit/i });
+    const submitButton = screen.getByRole('button', { name: /submit/i })
 
-    expect(submitButton).toBeDisabled();
-  });
+    expect(submitButton).toBeDisabled()
+  })
 
   it('should allow users to type into the fields and submit the form', async () => {
-    const firstNameField = screen.getByLabelText(/first name/i);
-    const lastNameField = screen.getByLabelText(/last name/i);
-    const emailField = screen.getByLabelText(/email/i);
-    const passwordField = screen.getByLabelText(/password/i);
-    const submitButton = screen.getByRole('button', { name: /submit/i });
+    const firstNameField = screen.getByLabelText(/first name/i)
+    const lastNameField = screen.getByLabelText(/last name/i)
+    const emailField = screen.getByLabelText(/email/i)
+    const passwordField = screen.getByLabelText(/password/i)
+    const submitButton = screen.getByRole('button', { name: /submit/i })
 
-    user.type(firstNameField, 'john doe');
-    user.type(lastNameField, 'john doe');
-    user.type(emailField, 'john@email.com');
-    user.type(passwordField, 'fakePassword123');
+    user.type(firstNameField, 'john doe')
+    user.type(lastNameField, 'john doe')
+    user.type(emailField, 'john@email.com')
+    user.type(passwordField, 'fakePassword123')
 
-    user.click(submitButton);
+    user.click(submitButton)
 
     await waitFor(() => {
       expect(onSubmit).toHaveBeenCalledWith({
@@ -35,36 +35,44 @@ describe('components/register-form', () => {
         lastName: 'john doe',
         email: 'john@email.com',
         password: 'fakePassword123',
-      });
-    });
-  });
+      })
+    })
+  })
 
   describe('with invalid inputs', () => {
-    beforeEach(async () => {
-      const firstNameField = screen.getByLabelText(/first name/i);
-      const lastNameField = screen.getByLabelText(/last name/i);
-      const emailField = screen.getByLabelText(/email/i);
-      const passwordField = screen.getByLabelText(/password/i);
+    const setupInvalidInputs = () => {
+      const firstNameField = screen.getByLabelText(/first name/i)
+      const lastNameField = screen.getByLabelText(/last name/i)
+      const emailField = screen.getByLabelText(/email/i)
+      const passwordField = screen.getByLabelText(/password/i)
 
-      await waitFor(() => user.type(firstNameField, 'gh'));
-      await waitFor(() => user.type(lastNameField, 'gh'));
-      await waitFor(() => user.type(emailField, 'gh'));
-      await waitFor(() => user.type(passwordField, 'gh'));
-    });
+      user.type(firstNameField, 'gh')
+      user.type(lastNameField, 'gh')
+      user.type(emailField, 'gh')
+      user.type(passwordField, 'gh')
+    }
 
-    it('should display error messages ', () => {
-      screen.getAllByRole('alert');
-    });
+    it('should display error messages ', async () => {
+      setupInvalidInputs()
 
-    it('should set the inputs aria-invalid to true', async () => {
-      expect(screen.getByLabelText(/first name/i)).toBeInvalid();
-      expect(screen.getByLabelText(/last name/i)).toBeInvalid();
-      expect(screen.getByLabelText(/email/i)).toBeInvalid();
-      expect(screen.getByLabelText(/password/i)).toBeInvalid();
-    });
+      await screen.findAllByRole('alert')
+    })
+
+    it('should make the inputs invalid', async () => {
+      setupInvalidInputs()
+
+      expect(await screen.findByLabelText(/first name/i)).toBeInvalid()
+      expect(await screen.findByLabelText(/last name/i)).toBeInvalid()
+      expect(await screen.findByLabelText(/email/i)).toBeInvalid()
+      expect(await screen.findByLabelText(/password/i)).toBeInvalid()
+    })
 
     it('should keep the button disabled', async () => {
-      expect(screen.getByRole('button', { name: /submit/i })).toBeDisabled();
-    });
-  });
-});
+      setupInvalidInputs()
+
+      expect(
+        await screen.findByRole('button', { name: /submit/i })
+      ).toBeDisabled()
+    })
+  })
+})
