b616d967f64f3d1a26ece8a6bf1309c4874f5252
miguelsndc <mglsndc@gmail.com>
2022-02-07 18:54:31 -0300

    feat(styles/globals): add font-smoothing and code fonts

--- src/styles/globals.ts
+++ src/styles/globals.ts
@@ -5,9 +5,14 @@ export const globalStyles = globalCss({
     margin: 0,
     padding: 0,
     boxSizing: 'border-box',
+    '-webkit-font-smoothing': 'antialiased',
+    '-moz-osx-font-smoothing': 'grayscale',
   },
   body: {
     fontFamily: '$roboto',
     color: '$gray900',
+  },
+  code: {
+    fontFamily: '$mono',
   },
 })
