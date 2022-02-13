8abec416e862d3202dead6015ab845124ebacce4
miguelsndc <mglsndc@gmail.com>
2022-02-13 13:16:01 -0300

    feat: firebase admin setup

---
+++ src/config/firebase-admin.ts
@@ -0,0 +1,20 @@
+import * as admin from 'firebase-admin'
+
+if (!admin.apps.length)
+  admin.initializeApp({
+    credential: admin.credential.cert({
+      type: process.env.FIREBASE_ADMIN_TYPE,
+      project_id: process.env.FIREBASE_ADMIN_PROJECT_ID,
+      private_key_id: process.env.FIREBASE_ADMIN_PRIVATE_KEY_ID,
+      private_key: process.env.FIREBASE_ADMIN_PRIVATE_KEY,
+      client_email: process.env.FIREBASE_ADMIN_CLIENT_EMAIL,
+      client_id: process.env.FIREBASE_ADMIN_CLIENT_ID,
+      auth_uri: process.env.FIREBASE_ADMIN_AUTH_URI,
+      token_uri: process.env.FIREBASE_ADMIN_TOKEN_URI,
+      auth_provider_x509_cert_url:
+        process.env.FIREBASE_ADMIN_AUTH_PROVIDER_X509_CERT_URL,
+      client_x509_cert_url: process.env.FIREBASE_ADMIN_CLIENT_X509_CERT_URL,
+    } as admin.ServiceAccount),
+  })
+
+export { admin }
