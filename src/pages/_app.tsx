7a7ec84cb25716ef3933aed65c9d94dfed7f0afd
miguelsndc <mglsndc@gmail.com>
2022-03-26 15:51:25 -0300

    feat: progress bar while loading pages

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,10 +1,17 @@
 import type { AppProps } from 'next/app'
+import NProgress from 'nprogress'
+import Router from 'next/router'
 import { Toaster } from 'react-hot-toast'
 import { globalStyles } from '../styles/globals'
 import { AuthProvider } from '../contexts/auth'
 import { EditorProvider } from '../contexts/editor'
 
 import '../config/firebase'
+import '../styles/nprogress.css'
+
+Router.events.on('routeChangeStart', () => NProgress.start())
+Router.events.on('routeChangeComplete', () => NProgress.done())
+Router.events.on('routeChangeError', () => NProgress.done())
 
 function MyApp({ Component, pageProps }: AppProps) {
   globalStyles()
