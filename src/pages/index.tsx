0740da040bba1e26e973797509542d22f18c7e40
miguelsndc <mglsndc@gmail.com>
2022-04-18 18:05:08 -0300

    feat(pages/home): welcome text

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -4,17 +4,35 @@ import { format } from 'date-fns'
 import { Layout, PostList } from '../components'
 import { firestore } from '../config/firebase'
 import { Post } from '../types'
+import { styled } from '../../stitches.config'
 
 type Props = {
   posts: Post[]
 }
 
+const Container = styled('div', {
+  paddingTop: '3rem',
+  paddingBottom: '4rem',
+
+  '& > h1': {
+    textAlign: 'center',
+    fontSize: 'clamp(3.5rem, 5vw, 5rem)',
+  },
+  '& > h2': {
+    textAlign: 'center',
+    fontSize: 'clamp(2rem, 5vw, 3rem)',
+    color: '$gray300',
+  },
+})
+
 function Home({ posts }: Props) {
   return (
     <Layout>
-      <div>
+      <Container>
+        <h1>Welcome to Johan !</h1>
+        <h2>See What&apos;s new:</h2>
         <PostList posts={posts} />
-      </div>
+      </Container>
     </Layout>
   )
 }
